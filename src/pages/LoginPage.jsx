27037bd90c513cb1f2ea6e05260dbdbb792489fb
Hugo <hugoo.ribeiro03@gmail.com>
2023-07-26 21:27:52 -0300

    versao tela login/cadastro

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,25 +1,64 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import {Button, ButtonRetorno,Container,Input} from "./../styles/StyleInicio"
 import logo from "../assets/logoTrackIt.png"
+import { useState } from "react";
+import axios from "axios";
 
 export default function LoginPage(){
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [desabilitar,setDesabilitar] = useState(false);
+    
+    const navigate = useNavigate();
+
+    function logar(event){
+
+        event.preventDefault();
+
+        setDesabilitar(true);
+
+        const login = {
+            email: email,
+            password: password
+        }
+
+        const promise = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login',login);
+        promise.then((resposta) => { navigate('/hoje')});
+        promise.catch( erro => alert(erro));
+    }
+
     return(
         <>
             <Container>
                 <img src={logo}/>
-                <Input
-                    type="email"
-                    placeholder="email"
-                    required
-                />
-                <Input
-                    type="password"
-                    placeholder="senha"
-                    required
-                />
-                <Button>Entrar</Button>
+                
+                    <Input
+                        data-test="email-input"
+                        type="email"
+                        placeholder="email"
+                        onChange={ e => setEmail(e.target.value)}
+                        value={email}
+                        disabled={desabilitar}
+                        required
+                    />
+                    <Input
+                        data-test="password-input"
+                        type="password"
+                        placeholder="senha"
+                        onChange={ e => setPassword(e.target.value)}
+                        value={password}
+                        disabled={desabilitar}
+                        required
+                    />
+                    <Button 
+                        onClick={logar} 
+                        disabled={desabilitar} 
+                        data-test="login-btn">
+                        Entrar
+                    </Button>
                 <Link to="/cadastro">
-                    <ButtonRetorno>Não tem uma conta? Cadastre-se!</ButtonRetorno>
+                    <ButtonRetorno data-test="signup-link" >Não tem uma conta? Cadastre-se!</ButtonRetorno>
                 </Link>
             </Container>
         </>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,35 +1,87 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import {Button, ButtonRetorno,Container,Input} from "./../styles/StyleInicio"
 import logo from "../assets/logoTrackIt.png"
+import { useState } from "react"
+import axios from "axios";
 
 export default function RegisterPage(){
+
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("");
+    const [password, setPassword] = useState("");
+    const [desabilitar,setDesabilitar] = useState(false);
+
+    const navigate = useNavigate();
+
+    function cadastrar(event){
+
+        event.preventDefault();
+
+        setDesabilitar(true);
+
+        const novaCadastro = {
+            email: email,
+            name: name,
+            image: image,
+            password: password
+        }
+
+        const promise = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up',novaCadastro);
+        promise.then((resposta) => { navigate('/')});
+        promise.catch( erro => alert(erro));
+    }
+
     return(
         <>
             <Container>
                 <img src={logo}/>
-                <Input
-                    type="email"
-                    placeholder="email"
-                    required
-                />
-                <Input
-                    type="password"
-                    placeholder="senha"
-                    required
-                />
-                <Input
-                    type="text"
-                    placeholder="nome"
-                    required
-                />
-                <Input
-                    type="url"
-                    placeholder="foto"
-                    required
-                />
-                <Button>Cadastrar</Button>
+                
+                    <Input
+                        data-test="email-input"
+                        type="email"
+                        placeholder="email"
+                        onChange={ e => setEmail(e.target.value)}
+                        value={email}
+                        required
+                        disabled={desabilitar}
+                    />
+                    <Input
+                        data-test="password-input"
+                        type="password"
+                        placeholder="senha"
+                        onChange={ e => setPassword(e.target.value)}
+                        value={password}
+                        required
+                        disabled={desabilitar}
+                    />
+                    <Input
+                        data-test="user-name-input"
+                        type="text"
+                        placeholder="nome"
+                        onChange={ e => setName(e.target.value)}
+                        value={name}
+                        required
+                        disabled={desabilitar}
+                    />
+                    <Input
+                        data-test="user-image-input" 
+                        type="url"
+                        placeholder="foto"
+                        onChange={ e => setImage(e.target.value)}
+                        value={image}
+                        required
+                        disabled={desabilitar}
+                    />
+                    <Button 
+                        onClick={cadastrar} 
+                        disabled={desabilitar} 
+                        data-test="signup-btn">
+                            Cadastrar
+                    </Button>
+                
                 <Link to="/">
-                    <ButtonRetorno>Já tem uma conta? Faça login!</ButtonRetorno>
+                    <ButtonRetorno data-test="login-link">Já tem uma conta? Faça login!</ButtonRetorno>
                 </Link>
             </Container>
         </>
